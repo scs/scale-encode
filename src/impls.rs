c0530f21fb04b0ef8c7db28b6fa182e66bd11d97
James Wilson <james@jsdw.me>
2022-11-24 18:12:50 +0000

    add some tests

--- src/impls.rs
+++ src/impls.rs
@@ -211,7 +211,7 @@ macro_rules! impl_encode_tuple {
             fn encode_as_type_to(&self, type_id: u32, types: &PortableRegistry, context: Context, out: &mut Vec<u8>) -> Result<(), Error> {
                 let ($($name,)*) = self;
 
-                let len = count_idents!($($t,)*);
+                const LEN: usize = count_idents!($($t,)*);
                 let ty = types
                     .resolve(type_id)
                     .ok_or_else(|| Error::new(context.clone(), ErrorKind::TypeNotFound(type_id)))?;
@@ -219,7 +219,7 @@ macro_rules! impl_encode_tuple {
                 // As long as the lengths line up, to the number of tuple items, we'll
                 // do our best to encode each inner type as needed.
                 match ty.type_def() {
-                    TypeDef::Tuple(tuple) if tuple.fields().len() == len => {
+                    TypeDef::Tuple(tuple) if tuple.fields().len() == LEN => {
                         let fields = tuple.fields();
                         let mut idx = 0;
                         $({
@@ -229,7 +229,7 @@ macro_rules! impl_encode_tuple {
                         })*
                         Ok(())
                     },
-                    TypeDef::Composite(composite) if composite.fields().len() == len => {
+                    TypeDef::Composite(composite) if composite.fields().len() == LEN => {
                         let fields = composite.fields();
                         let mut idx = 0;
                         $({
@@ -239,7 +239,7 @@ macro_rules! impl_encode_tuple {
                         })*
                         Ok(())
                     },
-                    TypeDef::Array(array) if array.len() == len as u32 => {
+                    TypeDef::Array(array) if array.len() == LEN as u32 => {
                         let mut idx = 0;
                         $({
                             let context = context.at(Location::idx(idx));
@@ -248,8 +248,31 @@ macro_rules! impl_encode_tuple {
                         })*
                         Ok(())
                     },
+                    TypeDef::Sequence(seq) => {
+                        let mut idx = 0;
+                        // sequences start with compact encoded length:
+                        Compact(LEN as u32).encode_to(out);
+                        $({
+                            let context = context.at(Location::idx(idx));
+                            $name.encode_as_type_to(seq.type_param().id(), types, context, out)?;
+                            idx += 1;
+                        })*
+                        Ok(())
+                    },
                     _ => {
-                        Err(Error::new(context, ErrorKind::WrongShape { actual: Kind::Tuple, expected: type_id }))
+                        // Tuple with 1 entry? before giving up, try encoding the inner entry instead:
+                        if LEN == 1 {
+                            // We only care about the case where there is exactly one copy of
+                            // this, but have to accomodate the cases where multiple copies and assume
+                            // that the compielr can easily optimise out this branch for LEN != 1.
+                            $({
+                                let context = context.at(Location::idx(0));
+                                $name.encode_as_type_to(type_id, types, context, out)?;
+                            })*
+                            Ok(())
+                        } else {
+                            Err(Error::new(context, ErrorKind::WrongShape { actual: Kind::Tuple, expected: type_id }))
+                        }
                     }
                 }
             }
@@ -356,6 +379,7 @@ impl_encode_like!(String as &str where |val| &*val);
 impl_encode_like!(Box<T> as &T where |val| &*val);
 impl_encode_like!(Arc<T> as &T where |val| &*val);
 impl_encode_like!(Rc<T> as &T where |val| &*val);
+impl_encode_like!(char as u32 where |val| *val as u32);
 impl_encode_like!(PhantomData<T> as () where |_val| ());
 impl_encode_like!(NonZeroU8 as u8 where |val| val.get());
 impl_encode_like!(NonZeroU16 as u16 where |val| val.get());
@@ -431,4 +455,127 @@ where
             Err(Error::new(context, ErrorKind::WrongShape { actual: Kind::Array, expected: type_id }))
         }
     }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use scale_info::TypeInfo;
+    use codec::Decode;
+    use std::fmt::Debug;
+
+	/// Given a type definition, return type ID and registry representing it.
+	fn make_type<T: TypeInfo + 'static>() -> (u32, PortableRegistry) {
+		let m = scale_info::MetaType::new::<T>();
+		let mut types = scale_info::Registry::new();
+		let id = types.register_type(&m);
+		let portable_registry: PortableRegistry = types.into();
+
+		(id.id(), portable_registry)
+	}
+
+    fn encode_type<V: EncodeAsType, T: TypeInfo + 'static>(value: &V) -> Result<Vec<u8>, Error> {
+        let (type_id, types) = make_type::<T>();
+        let context = Context::default();
+        let bytes = value.encode_as_type(type_id, &types, context)?;
+        Ok(bytes)
+    }
+
+    fn value_roundtrips_to<V: EncodeAsType, T: PartialEq + Debug + Decode + TypeInfo + 'static>(value: V, target: T) {
+        let bytes = encode_type::<_, T>(&value).expect("can encode");
+        let bytes_cursor = &mut &*bytes;
+        let new_target = T::decode(bytes_cursor).expect("can decode");
+
+        assert_eq!(bytes_cursor.len(), 0, "no bytes should be remaining");
+        assert_eq!(target, new_target, "value does not roundtrip and decode to target");
+    }
+
+    fn value_roundtrips<V: EncodeAsType + PartialEq + Debug + Decode + TypeInfo + 'static>(value: V) {
+        let bytes = encode_type::<_, V>(&value).expect("can encode");
+        let bytes_cursor = &mut &*bytes;
+        let new_value = V::decode(bytes_cursor).expect("can decode");
+
+        assert_eq!(bytes_cursor.len(), 0, "no bytes should be remaining");
+        assert_eq!(value, new_value, "value does not roundtrip and decode to itself again");
+    }
+
+
+    #[test]
+    fn numeric_roundtrips_encode_ok() {
+        macro_rules! int_value_roundtrip {
+            ($($val:expr; $ty:ty),+) => {$(
+                value_roundtrips_to($val, $val as i8);
+                value_roundtrips_to($val, $val as i16);
+                value_roundtrips_to($val, $val as i32);
+                value_roundtrips_to($val, $val as i64);
+                value_roundtrips_to($val, $val as i128);
+            )+}
+        }
+        macro_rules! uint_value_roundtrip {
+            ($($val:expr; $ty:ty),+) => {$(
+                value_roundtrips_to($val, $val as u8);
+                value_roundtrips_to($val, $val as u16);
+                value_roundtrips_to($val, $val as u32);
+                value_roundtrips_to($val, $val as u64);
+                value_roundtrips_to($val, $val as u128);
+            )+}
+        }
+        macro_rules! int_value_roundtrip_types {
+            ($($val:expr),+) => {$(
+                int_value_roundtrip!($val; i8);
+                int_value_roundtrip!($val; i16);
+                int_value_roundtrip!($val; i32);
+                int_value_roundtrip!($val; i64);
+                int_value_roundtrip!($val; i128);
+            )+}
+        }
+        macro_rules! uint_value_roundtrip_types {
+            ($($val:expr),+) => {$(
+                uint_value_roundtrip!($val; u8);
+                uint_value_roundtrip!($val; u16);
+                uint_value_roundtrip!($val; u32);
+                uint_value_roundtrip!($val; u64);
+                uint_value_roundtrip!($val; u128);
+            )+}
+        }
+        uint_value_roundtrip_types!(0, 1, 100, 127);
+        int_value_roundtrip_types!(-127, -100, 0, 1, 100, 127);
+    }
+
+    #[test]
+    fn out_of_range_numeric_roundtrips_fail_to_encode() {
+        encode_type::<_, u8>(&1234u16).unwrap_err();
+        encode_type::<_, i8>(&129u8).unwrap_err();
+        encode_type::<_, u8>(&-10i8).unwrap_err();
+    }
+
+    #[test]
+    fn mixed_tuples_roundtrip_ok() {
+        value_roundtrips(());
+        value_roundtrips((12345,));
+        value_roundtrips((123u8, true));
+        // Decode isn't implemented for &str:
+        value_roundtrips((123u8, true, "hello".to_string()));
+        // Decode isn't implemented for `char` (but we treat it as a u32):
+        value_roundtrips((123u8, true, "hello".to_string(), 'a' as u32));
+        value_roundtrips((123u8, true, "hello".to_string(), 'a' as u32, 123_000_000_000u128));
+    }
+
+    #[test]
+    fn sequences_roundtrip_into_eachother() {
+        // Tuples can turn to sequences or arrays:
+        value_roundtrips_to((1u8, 2u8, 3u8), vec![1u8, 2u8, 3u8]);
+        value_roundtrips_to((1u8, 2u8, 3u8), [1u8, 2u8, 3u8]);
+
+        // Even when inner types differ but remain compatible:
+        value_roundtrips_to((1u8, 2u8, 3u8), vec![1u128, 2u128, 3u128]);
+        value_roundtrips_to((1u8, 2u8, 3u8), vec![(1u128,), (2u128,), (3u128,)]);
+        value_roundtrips_to(((1u8,), (2u8,), 3u8), vec![1u128, 2u128, 3u128]);
+
+        // tuples can also encode to structs of same lengths (with inner type compat):
+        #[derive(Debug, scale_info::TypeInfo, codec::Decode, PartialEq)]
+        struct Foo { a: (u32,), b: u64, c: u128 }
+        value_roundtrips_to((1u8, 2u8, 3u8), Foo { a: (1,), b: 2, c: 3 });
+    }
+
 }
